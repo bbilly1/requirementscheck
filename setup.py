d95aa8a35a57a7513ba234583927cf6de5392c33
Simon <simobilleter@gmail.com>
2025-02-25 10:33:34 +0700

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="RequirementsCheck",
-    version="0.0.5",
+    version="0.0.6",
     author="Simon",
     author_email="simobilleter@gmail.com",
     description="A CLI utility to check and update Python package versions in requirements.txt files",
